45ec61074a158e357ae9c8fb3a136cebfd92b737
Yerinnnn <shinyerin0216@gmail.com>
2024-06-24 17:24:09 +0900

    feat : "추가 helloApi 메소드, Hello 클래스"
    API 방식 실습
    @ResponseBody 객체 반환
    - @ResponseBody 를 사용하고, 객체를 반환하면 객체가 JSON으로 변환됨

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -12,6 +12,7 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "Hello!");
         return "hello";     // viewResolver가 "resources:templates/" + "{ViewName}" + ".html"을 찾음
+        // 크롬에서 마우스 우클릭 후 [페이지 소스 보기]를 확인해보면, hello 파일 코드가 나옴
     }
 
     @GetMapping("hello-mvc")    // /hello-mvc를 요청하면 이 메소드가 실행됨
@@ -20,11 +21,35 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+        // 크롬에서 마우스 우클릭 후 [페이지 소스 보기]를 확인해보면, hello-template 파일 코드가 나옴
     }
 
     @GetMapping("hello-string")
     @ResponseBody   // http의 응답 body부에 "hello" + name을 직접 넣어줄 때 사용
     public String helloString(@RequestParam("name") String name) {
         return "hello " + name;
+        // [페이지 소스 보기]를 확인해보면, html 태그 없이 텍스트만 나옴
+        // (view 없이 http의 body에 직접 넣어줬기 때문)
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // [페이지 소스 보기]를 확인해보면, json 형식으로 나옴
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
