bdd60532b644727e3ddac5ca9624b2555544550d
Yerinnnn <shinyerin0216@gmail.com>
2024-06-24 14:49:11 +0900

    feat : "추가 helloMvc 메소드"
    MVC와 템플릿 엔진 실습

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,12 +3,22 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("hello")    // /hello를 요청하면 이 메소드가 실행됨
     public String hello(Model model){
         model.addAttribute("data", "Hello!");
         return "hello";     // viewResolver가 "resources:templates/" + "{ViewName}" + ".html"을 찾음
+    }
+
+    @GetMapping("hello-mvc")    // /hello-mvc를 요청하면 이 메소드가 실행됨
+    // RequestParam의 required 옵션의 기본값이 true이기 때문에, /hello-mvc?name={name}으로 값을 꼭 넘겨줘야 함!
+    // required = false라고 옵션을 준 후에, name값을 주지 않으면, html의 "name" 부분이 null로 출력됨
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
